58f802962aa18dea306b25803fd12feb9d8fc64d
Donarti10 <123815608+Donarti10@users.noreply.github.com>
2023-05-25 20:32:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from "react";
-import './app.scss'
-import {dataDomain} from './data'
+import "./app.scss";
+import { dataDomain } from "./data";
 
 function App() {
   return (
     <div className="container">
       <div className="card">
         {dataDomain.map((card) => {
-            return(
-              <div className="cards" key={card.id} >
-                <div className="career-card">
-                   <div className="career-icon" style={{ content: `url(${card.icon})` }}></div>
-                 </div> 
-                 <div className="career-items">
-                   <h3>{card.title}</h3>
-  
+          return (
+            <div className="cards" key={card.id}>
+              <div className="career-card">
+                <div
+                  className="career-icon"
+                  style={{ content: `url(${card.icon})` }}
+                ></div>
+              </div>
+              <div className="career-items">
+                <h3>{card.title}</h3>
+
                 <p>{card.text}</p>
-                </div>
-                </div>
-            )
+              </div>
+            </div>
+          );
         })}
-        </div>
+      </div>
     </div>
   );
 }
